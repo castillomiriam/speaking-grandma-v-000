0d8dd9b47a23c2dccfe82ae25af3a70149b60461
Miriam Castillo <castillo.m.mc@gmail.com>
2018-04-03 21:35:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 
 def speak_to_grandma (words)
 if words == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
 elsif words == words.upcase && speak != "I LOVE YOU TOO PUMPKIN!"
-  puts "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
 else
-  puts "HUH!? SPEAK UP, SONNY!"
+  return "HUH!? SPEAK UP, SONNY!"
 end
 end
